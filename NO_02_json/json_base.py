4f2c5e525e75486c69accc13fbbe574aa72d1711
BigDataMao <mxw712@gmail.com>
2023-02-20 22:36:19 +0800

    修改表格格式

--- NO_02_json/json_base.py
+++ NO_02_json/json_base.py
@@ -1,7 +1,35 @@
 import pandas as pd
 
+
+def p():
+    print(df, '\n\n')
+
+
 df = pd.read_json("../data/input/json/sites.json")
-print(df, '\n\n')
+p()
 
 df.index = (['第一行', '第二行', '第三行'])
-print(df)
+p()
+
+df.index = range(1, 4, 1)
+p()
+
+a = []
+for i in range(1, 4, 1):
+    a.append(f'高级的第{i}行')
+df.index = a
+# 格式化输出,但是不整齐
+print(f"{'id':<10}{'name':<10}{'likes':<10}{'url':<20}")
+for index, row in df.iterrows():
+    print(f"{row['id']:<10}"
+          f"{row['name']:<10}"
+          f"{row['likes']:<10}"
+          f"{row['url']:<20}")
+
+# 整齐的格式化,实际测试还是不整齐,放弃
+# 据说有个prettytable包
+from tabulate import tabulate
+
+print(tabulate(df,headers=df.columns.tolist(),
+               tablefmt='psql',stralign='left',
+               numalign='left', showindex=False))
